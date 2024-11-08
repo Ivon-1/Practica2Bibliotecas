f23a4d40fc614eec261cff49b9de4ce150ce760b
sergi <sergi@192.168.1.100>
2024-11-08 11:59:46 +0100

    Modelo Socio

--- src/modelo/Socio.java
+++ src/modelo/Socio.java
@@ -9,5 +9,92 @@ package modelo;
  * @author sergi
  */
 public class Socio {
+
+    private int id;
+    private String socio;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String dni;
+    private String telefono;
+    private boolean pago;
     
+
+    // constructor
+    public Socio(int id, String socio, String nombre, String apellido, String correo, String dni, String telefono, boolean pago) {
+        this.id = id;
+        this.socio = socio;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.pago = pago;
+    }
+
+    // getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSocio() {
+        return socio;
+    }
+
+    public void setSocio(String socio) {
+        this.socio = socio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isPago() {
+        return pago;
+    }
+
+    public void setPago(boolean pago) {
+        this.pago = pago;
+    }
+
 }
