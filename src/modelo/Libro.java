c5fc1fd9f9a1a71aa04033e52065b0e33699d359
SMartinezDAM <smakapmartinez@gmail.com>
2024-11-08 12:12:03 +0100

    Modelo Libros

---
+++ src/modelo/Libro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author sergi
+ */
+public class Libro {
+    private String ISBN;
+    private String titulo;
+    private int numSerie;
+    private int precio;
+    private String estado;
+    private String editorial;
+}
