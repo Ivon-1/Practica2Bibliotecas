2fdebabf4b43a3c6724915c7a35d46d0f850c88b
sergi <sergi@192.168.1.100>
2024-11-08 12:14:15 +0100

    Libros constructor / get-set

--- src/modelo/Libro.java
+++ src/modelo/Libro.java
@@ -15,4 +15,67 @@ public class Libro {
     private int precio;
     private String estado;
     private String editorial;
+
+    // constructor
+    public Libro(String ISBN, String titulo, int numSerie, int precio, String estado, String editorial) {
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.numSerie = numSerie;
+        this.precio = precio;
+        this.estado = estado;
+        this.editorial = editorial;
+    }
+    
+    // getter y setter 
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNumSerie() {
+        return numSerie;
+    }
+
+    public void setNumSerie(int numSerie) {
+        this.numSerie = numSerie;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+    
+    
+    
 }
