b9fd9772af94df1c8098eed69387a77252d5c1d6
SMartinezDAM <smakapmartinez@gmail.com>
2024-11-22 01:08:26 +0100

    Trabajadores/Administracion modelo y vista

---
+++ src/controlador/MostrarTrabajadorController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import javax.swing.table.DefaultTableModel;
+import modelo.TrabajadorModelo;
+import vista.AdministracionView;
+import vista.MenuView;
+
+/**
+ *
+ * @author sergi
+ */
+public class MostrarTrabajadorController {
+    private TrabajadorModelo modelo_trabajador;
+    private AdministracionView vista_trabajador;
+    private DefaultTableModel datos_tabla_trabajador;
+    private MenuView vista_menu;
+}
